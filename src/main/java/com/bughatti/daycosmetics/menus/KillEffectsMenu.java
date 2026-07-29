8d008d297362e3ad25c8cfce4218cd979eeb5f45
bugh4tti <quieroamiabuu@gmail.com>
2026-07-29 17:42:27 -0300

    Add KillEffectsMenu class to manage kill effects

---
+++ src/main/java/com/bughatti/daycosmetics/menus/KillEffectsMenu.java
@@ -0,0 +1,33 @@
+package com.bughatti.daycosmetics.menus;
+
+import com.bughatti.daycosmetics.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class KillEffectsMenu {
+
+    public static void open(Player player) {
+        Main plugin = Main.getInstance();
+        Inventory inv = Bukkit.createInventory(null, 54,
+                plugin.getConfig().getString("menus.killeffects.title"));
+
+        MenuUtils.fillBorders(inv);
+
+        ConfigurationSection sec = plugin.getConfig().getConfigurationSection("killeffects");
+        for (String key : sec.getKeys(false)) {
+            String path = "killeffects." + key + ".";
+            Material mat = Material.valueOf(plugin.getConfig().getString(path + "material"));
+            String name = plugin.getConfig().getString(path + "name");
+            int slot = plugin.getConfig().getInt(path + "slot");
+
+            ItemStack item = MenuUtils.namedItem(mat, name);
+            inv.setItem(slot, item);
+        }
+
+        player.openInventory(inv);
+    }
+}
